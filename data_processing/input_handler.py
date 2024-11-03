8e493e2a6449ffe1af6641af2d922f151549221b
Danila Shiryaev <dshirya@gmail.com>
2024-11-03 01:14:19 -0500

    1.2

--- data_processing/input_handler.py
+++ data_processing/input_handler.py
@@ -1,7 +1,34 @@
 import pandas as pd
 import sys
+import os
+import click
 
+def list_excel_files():
+    """
+    Lists Excel files in the current directory and prompts the user to select one.
+    
+    Returns:
+        str: The file path of the selected Excel file, or None if no valid file is chosen.
+    """
+    # List all Excel files in the current directory
+    excel_files = [f for f in os.listdir() if f.endswith('.xlsx') or f.endswith('.xls')]
+    if not excel_files:
+        click.echo("No Excel files found in the current directory.")
+        return None
 
+    # Display the files with an index
+    click.echo("Select an Excel file by number:")
+    for i, file in enumerate(excel_files, start=1):
+        click.echo(f"{i}. {file}")
+
+    # Prompt the user to select a file by index
+    choice = click.prompt("Enter the number of the file", type=int)
+    if 1 <= choice <= len(excel_files):
+        return excel_files[choice - 1]
+    else:
+        click.echo("Invalid choice. Exiting.")
+        return None
+    
 def input_handler(file_path):
     excel_file = pd.ExcelFile(file_path)
     sheet_names = excel_file.sheet_names
--- main.py
+++ main.py
@@ -1,31 +1,10 @@
 import click
-import os
-from data_processing.input_handler import input_handler
+from data_processing.input_handler import input_handler, list_excel_files
 from data_processing.coord_excel_handler import *
 from data_processing.make_periodic_table import periodic_table
 from make_data import *
 from display_data import *
 from data_processing.compound_object import pick_what_separate  
-
-def list_excel_files():
-    # List all Excel files in the current directory
-    excel_files = [f for f in os.listdir() if f.endswith('.xlsx') or f.endswith('.xls')]
-    if not excel_files:
-        click.echo("No Excel files found in the current directory.")
-        return None
-
-    # Display the files with an index
-    click.echo("Select an Excel file by number:")
-    for i, file in enumerate(excel_files, start=1):
-        click.echo(f"{i}. {file}")
-
-    # Prompt the user to select a file by index
-    choice = click.prompt("Enter the number of the file", type=int)
-    if 1 <= choice <= len(excel_files):
-        return excel_files[choice - 1]
-    else:
-        click.echo("Invalid choice. Exiting.")
-        return None
 
 @click.command()
 def main():
