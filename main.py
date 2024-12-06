8b4a5ea27f46b0e57aaa7e40357e3383d46924ce
Danila Shiryaev <dshirya@gmail.com>
2024-12-06 15:43:35 -0500

    small changes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import click
-import os
 from data_processing.input_handler import input_handler, list_excel_files
-from data_processing.coord_excel_handler import *
+from data_processing.coord_excel_handler import *  # noqa: F403
 from data_processing.make_periodic_table import periodic_table
-from display_data import *
+from display_data import *  # noqa: F403
 from make_data.neighbors_search import recommendation_system, save_recommendations_to_excel
 from make_data import make_compound_data  # Import the consolidated function
 from data_processing.compound_object import pick_what_separate
@@ -22,8 +21,8 @@ def main():
 
     # Process the selected Excel file
     user_input_sheet_numbers = input_handler(file_path)
-    coord_df, coord_sheet_name = excel_to_dataframe()
-    element_dict = create_element_dict(coord_df)
+    coord_df, coord_sheet_name = excel_to_dataframe()  # noqa: F405
+    element_dict = create_element_dict(coord_df)  # noqa: F405
     periodic_table_ax = periodic_table(coord_df, coord_sheet_name)
 
     # Generate the compound data dynamically
@@ -38,14 +37,14 @@ def main():
                 compound.separate_by_element(target_element)
             compounds_binary.sort(key=lambda x: f"(with {target_element})" in x.structure)
 
-            display_binary_data_type(periodic_table_ax, compounds_binary, element_dict, coord_sheet_name)
+            display_binary_data_type(periodic_table_ax, compounds_binary, element_dict, coord_sheet_name)  # noqa: F405
             top_n = 50  # Specify the number of top recommendations to save
             recommendations = recommendation_system(compounds_binary, target_element, element_dict, top_n)
             save_recommendations_to_excel(recommendations, target_element)
             click.echo("Binary compounds processed.")
         else:
             # If no target element is selected, just display the binary data
-            display_binary_data_type(periodic_table_ax, compounds_binary, element_dict, coord_sheet_name)
+            display_binary_data_type(periodic_table_ax, compounds_binary, element_dict, coord_sheet_name)  # noqa: F405
             click.echo("No target element selected. Binary structures visualization saved to the /plots folder.")
     # Process ternary compounds if any
     if compounds_ternary:           
@@ -61,13 +60,13 @@ def main():
 
             compounds_ternary.sort(key=lambda x: f"(with {formatted_target})" in x.structure)
 
-            display_ternary_data_type(periodic_table_ax, compounds_ternary, element_dict, coord_sheet_name)
+            display_ternary_data_type(periodic_table_ax, compounds_ternary, element_dict, coord_sheet_name)  # noqa: F405
             top_n = 50
             recommendations = recommendation_system(compounds_ternary, target_elements, element_dict, top_n)
             save_recommendations_to_excel(recommendations, formatted_target)
             click.echo("Ternary compounds processed.")
         else:
-            display_ternary_data_type(periodic_table_ax, compounds_ternary, element_dict, coord_sheet_name)
+            display_ternary_data_type(periodic_table_ax, compounds_ternary, element_dict, coord_sheet_name)  # noqa: F405
             click.echo("No target element selected. Ternary structures visualization saved to the /plots folder.")
     return 0
 
