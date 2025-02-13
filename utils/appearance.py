14c6047e632547b8e117df25a9a5b4d796ec1512
Danila Shiryaev <dshirya@gmail.com>
2025-02-13 15:01:23 -0500

    Update appearance.py

--- utils/appearance.py
+++ utils/appearance.py
@@ -11,27 +11,15 @@ marker_size = 200  # Default size for scatter markers
 # Colors for different structures
 colors = [
    
-    "#027608",  # Clover (2, 118, 8)
-    "#1dace6",  # Cerulean (29, 172, 214)
     "#c3121e",  # Sangre (195, 18, 30)
-    
-    "#9966cc",  # Amethyst (153, 102, 204)
-    "#9c5300",  # Cocoa (156, 83, 0)
     "#0348a1",  # Neptune (3, 72, 161)
     "#ffb01c",  # Pumpkin (255, 176, 28)
-    "#ff4500"   # Orange Red (255, 69, 0)
+    "#027608",  # Clover (2, 118, 8)
+    "#1dace6",  # Cerulean (29, 172, 214)
+    "#9c5300",  # Cocoa (156, 83, 0)
+    "#9966cc",  # Amethyst (153, 102, 204)
     
 ]
-
-
-    # "#c3121e",  # Sangre (195, 18, 30)
-    # "#0348a1",  # Neptune (3, 72, 161)
-    # "#ffb01c",  # Pumpkin (255, 176, 28)
-    # "#027608",  # Clover (2, 118, 8)
-    # "#1dace6",  # Cerulean (29, 172, 214)
-    # "#9c5300",  # Cocoa (156, 83, 0)
-    # "#9966cc",  # Amethyst (153, 102, 204)
-    # "#ff4500"   # Orange Red (255, 69, 0)
 
 
 shape_linewidth = 2  # Line width for shapes (circles/rectangles)
